17756db29ef99640bfc32055ea98e0ade5d6dccc
Alexander Bulatov <aleks40k@yandex.ru>
2026-01-10 17:20:55 +0300

    feat: finish led roulette

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,24 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let half_period = 50_u16;
+
+    leds[0].on().ok();
+    let mut cur: usize = 0;
+    loop {
+        let prev: usize = cur;
+        cur = (cur + 1) % 8;
+        leds[cur].on().ok();
+        delay.delay_ms(half_period);
+        leds[prev].off().ok();
+        delay.delay_ms(half_period);
+    }
 }
+
+
